cef723557f861b8c0da4943e5d109e98d066be92
Micah Martin <micah@8thlight.com>
2009-05-30 03:21:25 -0500

    added some fitnesse tests

---
+++ fixtures/lights_out.rb
@@ -0,0 +1,48 @@
+require 'drb'
+
+module Fixtures
+
+
+  class LightsOut
+
+    def start_production
+      Thread.new { system "jruby -S limelight open -d 9000 ../lights_out" }
+      DRb.start_service
+      while(@studio.nil?)
+        @studio = DRbObject.new(nil, "druby://127.0.0.1:9000")
+        begin
+          @studio['lights_out']
+        rescue
+          @studio = nil
+          sleep(0.1)
+        end
+      end
+      sleep(4)
+      production = @studio['lights_out']
+      stage = production.theater['default']
+      @scene = stage.current_scene
+      return true
+    end
+
+    def press(name)
+      @scene.find(name).mouse_clicked(nil)
+      sleep(1)
+      return true
+    end
+
+    def level
+      return @scene.level.text
+    end
+
+    def status
+      return @scene.status.text
+    end
+
+    def close_production
+      @studio.shutdown
+      return true
+    end
+
+  end
+
+end
