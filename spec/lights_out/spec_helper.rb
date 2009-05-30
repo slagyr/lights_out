01e9d6598161997ab66b53862f5b6b2b9174b1d3
Micah Martin <micah@8thlight.com>
2009-05-30 02:45:32 -0500

    added specs for the board scene

---
+++ spec/lights_out/spec_helper.rb
@@ -0,0 +1,5 @@
+require File.expand_path(File.dirname(__FILE__) + "/../spec_helper")
+
+require 'limelight'
+$PRODUCTION_PATH = File.expand_path(File.dirname(__FILE__) + "/../../lights_out")
+require 'limelight/specs/spec_helper'
